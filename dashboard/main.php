0739c4341641c3908940e88caf7b1bc337f47881
yumi <s9101505@gmail.com>
2018-01-08 20:55:02 +0800

    美化登入註冊畫面

--- dashboard/main.php
+++ dashboard/main.php
@@ -1,8 +1,104 @@
-<!-- 設定網頁編碼為UTF-8 -->
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<form name="form" method="POST" action="connect.php">
-帳號：<input type="text" name="id" /> <br>
-密碼：<input type="password" name="pw" /> <br>
-<input type="submit" name="button" value="登入" />&nbsp;&nbsp;
-<a href="register.php">申請帳號</a>
-</form>
+
+
+
+  <head>
+    <link rel="shortcut icon" type="image/x-icon" href="../image/favicon.ico">
+    
+    <title>分支教學網</title>
+
+    <meta charset="utf-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <script src="http://code.jquery.com/jquery.min.js"></script>
+
+    <!-- Latest compiled and minified CSS -->
+   
+    
+    <link href="..\boostrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="..\boostrap/css/bootstrap-theme.css" rel="stylesheet">
+    <script src="..\boostrap/js/bootstrap.min.js" rel="stylesheet"></script>
+    <link href="..\css_code/btn.css" rel="stylesheet"> 
+    <link href="..\css_code/mystyle.css" rel="stylesheet">
+    <link href="..\css_code/JapanStyle.css" rel="stylesheet"> 
+	<script src="..\js_code/myjs.js" rel="stylesheet"></script>
+	
+  	<script type="text/javascript" src="../alertify.js-0.3.11/lib/alertify.js"></script>
+	<link rel="stylesheet" href="../alertify.js-0.3.11/themes/alertify.core.css" />
+	<link rel="stylesheet" href="../alertify.js-0.3.11/themes/alertify.default.css" />
+  
+  </head>
+
+  <body>
+      <div class="row">
+		<div class="col-md-3">
+			<img src = "../image/banner.jpg" class="center-block" >
+		</div>
+		<div class="col-md-7 center-block banner" >
+			<div class="h4  pre text-center">
+			<div class="japanstyle16">分支概念教學網站</div>
+			主旨為希望透過此教學網站，使大家更清楚明白有關於分支的概念，並應用於實作上面<br><br>
+			</div>
+		</div>
+		
+		
+	</div>
+
+    <article>
+		<nav class="btn-bar nav-dark ">
+			<a href="index.php" class="btn btn-glass">
+				home
+			</a>
+			<a href="if-else_study.php" class="btn btn-glass btn-primary">
+				<i class="fa fa-fw fa-lg fa-chevron-right"></i> If-else
+			</a>
+			<a href="switch-case_study.php" class="btn btn-glass btn-success">
+				<i class="fa fa-fw fa-lg fa-check"></i> Switch-case
+			</a>
+			<a href="ternary_study.php" class="btn btn-glass btn-warning">
+				<i class="fa fa-fw fa-lg fa-exclamation "></i> 
+				ternary conditional operator
+			</a>
+			<a href="games.php" class="btn btn-glass btn-danger">
+				<i class="fa fa-fw fa-lg fa-times"></i> games
+			</a>
+			<a href="aboutus.php" class="btn btn-glass btn-info">
+				<i class="fa fa-fw fa-lg fa-info "></i> browsing history
+			</a>
+		</nav>
+	</article>
+	
+	<div class="row">
+	    <div class="col-md-10 col-md-offset-1">
+			<hr class="style-four"> 
+		</div>
+		<div class="col-md-8 col-md-offset-2 text-center">
+		<form name="form" method="POST" action="connect.php">
+		帳號：<input type="text" name="id" /> <br>
+		密碼：<input type="password" name="pw" /> <br>
+		<input type="submit" name="button" value="登入" />&nbsp;&nbsp;
+		<a href="register.php">申請帳號</a>
+		</form>
+		</div>
+	</div>
+
+    <div class="row">
+      <div class="col-md-10 col-md-offset-1">
+        <hr class="style-four"> 
+      </div>
+      <div class="col-md-8 col-md-offset-2 h5 text-center">
+        台北市立大學資訊科學系4年級<br>
+        <span class="glyphicon glyphicon-copyright-mark " aria-hidden="true"></span>2018 Branch-Backend
+
+      </div>
+
+    </div>
+
+  <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>-->
+
+
+  </body>
+
+</html>
